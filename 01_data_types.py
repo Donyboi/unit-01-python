4c5d0f37c8d7b4e81ea5d7471ae6b6f0b7b22849
Donyboi <djean2@brooklynsteamcenter.org>
2025-09-11 10:51:01 -0400

    .

---
+++ 01_data_types.py
@@ -0,0 +1,67 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+don_float=15.6
+
+
+
+don_intger=int(don_float)
+print(don_float)
+print(don_intger)
+
+#turn a float into a integer
+
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+number =float(input("15"))
+if number > 0:
+    print=("the number is positve")
+elif number <0:
+    print=("the number is negative")
+else:
+    print=("the number is zero")
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+x={
+    "apple":7,
+    "mango":8
+}
+print(x["apple"])
+
+#i made a dictonary that prints apple.
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+
+"""
+TASK 6:
+
+Create a list of your favorite subjects (as strings). 
+Use the join() function to combine all subjects into a single string separated by commas.
+Then create another version using " - " as the separator.
+Print both the original list and both joined strings.
+"""
