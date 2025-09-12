bc9277fab07e6b32bbe9fc5b3d522dd9e2aea870
Donyboi <djean2@brooklynsteamcenter.org>
2025-09-12 09:42:58 -0400

    python

--- 01_data_types.py
+++ 01_data_types.py
@@ -4,6 +4,9 @@ Create a float variable, and then convert it to an integer
 Print both the original variable and the converted integer.
 
 """
+print("------task1------")
+print()
+print()
 don_float=15.6
 
 
@@ -11,6 +14,10 @@ don_float=15.6
 don_intger=int(don_float)
 print(don_float)
 print(don_intger)
+
+print()
+print()
+print("------task1------")
 
 #turn a float into a integer
 
@@ -20,13 +27,22 @@ TASK 2:
 Write code that takes a number as input and prints whether 
 it's positive, negative, or zero using if-elif-else statements.
 """
-number =float(input("15"))
+print("------task2------")
+print()
+print()
+
+
+number =float(input("enter a number"))
 if number > 0:
-    print=("the number is positve")
-elif number <0:
-    print=("the number is negative")
+    print("the number is positve")
+elif number < 0:
+    print("the number is negative")
 else:
-    print=("the number is zero")
+    print("the number is zero")
+
+print("------task2------")
+print()
+print()
 
 """
 TASK 3:
@@ -34,20 +50,45 @@ TASK 3:
 Write code that takes two numbers as input (an integer and a float), 
 performs addition, subtraction, multiplication, and division, and prints the results.
 """
+print()
+print("------task3------")
+print()
+print()
+
+dony3=input("give ne a float:")
+
+dony3=float(dony3)
+
+print(type(dony3))
+
+dony4=input("give me a integer:")
+
+dony4=int(dony4)
+print(type(dony4))
 
 
-
+print("------task3------")
+print()
+print()
 """
 TASK 4:
 
 Create a dictionary with keys as fruit names and values as their respective quantities. 
 Then print out the quantity of one of the fruits.
 """
+print("------task4------")
+print()
+print()
+
 x={
     "apple":7,
     "mango":8
 }
 print(x["apple"])
+
+print("------task3------")
+print()
+print()
 
 #i made a dictonary that prints apple.
 """
