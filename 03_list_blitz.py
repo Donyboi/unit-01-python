86709eb7f7ea1f7b44c25d5fa43d597d778ff3d2
Donyboi <djean2@brooklynsteamcenter.org>
2025-09-15 10:50:47 -0400

    list2

--- 03_list_blitz.py
+++ 03_list_blitz.py
@@ -2,24 +2,52 @@
 Task 1: Create a list
 Create a list with 4 elements and print it.
 """
+print()
+print("-------task1----")
+print()
+print()
+
+lis = [False,"hello",12 "people"]
+print(lis)
+
 
 """
 Task 2: Add Element to a List
 Add an element to the end of the list. Print the updated 
 list.
 """
+print()
+print("-------task2----")
+print()
+print()
+
+li = [True,31]
+li.append("Eyes")
+print(li)
 
 """
 Task 3: Remove Element from a List
 Remove a specific element from the list. Print the 
 updated list.
 """
+print()
+print("-------task3----")
+print()
+print()
+
+l = ["mango",True,0.2]
+l.remove("mango")
+print(1)
 
 """
 Task 4: Modify Element in a List
 Modify an element at a specific index with a new value. 
 Print the updated list.
 """
+print()
+print("-------task3----")
+print()
+print()
 
 """
 Task 5: Append Multiple Elements to a List
