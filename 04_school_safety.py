86267aa4e2580c2f9dfb545fdd30534f41946393
Donyboi <donaldsonjean189@gmail.com>
2025-09-18 13:25:18 -0400

    task

---
+++ 04_school_safety.py
@@ -0,0 +1,37 @@
+
+name = input("what is your name: ")
+
+
+if pass_1 == "yes":
+
+    pass_1 = input("do you have a pass:")
+
+    time_pass = int(input("what time was the pass issued:"))
+
+    what_time_is_it = int(input("what time is it now:"))
+
+    were_are_going = input("where are we going:")
+    
+    print(time_pass)
+    print(what_time_is_it) 
+    if what_time_is_it - time_pass <= 2:
+        print("you can go")
+    else:
+        print("your pass has expired")
+    if were_are_going == "nurse" or were_are_going == "bathroom" or were_are_going =="nurse":
+        print("you can go")
+
+
+
+        
+
+
+
+
+
+else:
+    print("no you dont have a pass")
+
+
+
+
