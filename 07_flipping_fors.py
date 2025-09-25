a1992db0a9663ae3f73429bdde32bf5863e7d57d
Donyboi <donaldsonjean189@gmail.com>
2025-09-25 10:40:26 -0400

    forssssss

---
+++ 07_flipping_fors.py
@@ -0,0 +1,60 @@
+"""
+Exercise 1:
+Write a program to print each character of a given string using a for loop.
+"""
+print()
+print("-------task1----")
+print()
+print()
+bob = "howdy mate"
+for tommy in bob:
+    print(tommy)
+
+
+"""
+Exercise 2:
+Write a program to calculate the sum of elements in a list using a for loop.
+"""
+print()
+print("-------task2----")
+print()
+print()
+buttons = [1, 2, 3, 4, 5]
+total = 0
+for counting in buttons:
+    total += counting
+print(total)
+
+
+
+
+"""
+Exercise 3:
+Write a program to print the length of each word in a sentence using a for loop and a list.
+"""
+print()
+print("-------task3----")
+print()
+
+FREZA = list("YOU, M0NKEY")
+
+for goku in FREZA:
+    print(goku)
+
+
+"""
+Excercise 4:
+Write a program that creates a dictionary (atleast 4 key:value pairs) and then
+iterates through a dictionary and prints the result
+
+In a comment, answer the following, what do you notice about the output of your code?
+Is it what you expected?
+"""
+
+my_dict = {
+    "bob": 3,
+    "dave": 2,
+    "carly": 7,
+    
+}
+
