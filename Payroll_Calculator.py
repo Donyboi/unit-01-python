d3f23e865a826c301dacb440411f8e0aa163c5a9
Donyboi <donaldsonjean189@gmail.com>
2025-10-01 09:30:51 -0400

    to do

--- Payroll_Calculator.py
+++ Payroll_Calculator.py
@@ -4,18 +4,18 @@ total_payroll = 0
 
 for i in range(employees):
     print(f"Employee {i+1}")
-    employee_num = float(input("Enter  number employees : "))
-    hours_worked = float(input("Enter number of hours worked: "))
-    overtime_hours = float(input("Enter number of overtime hours worked: "))
-    hourly_pay = float(input("Enter hourly pay rate: "))
+    employee_num = float(input("Enter  number employees : ")) # Employee number
+    hours_worked = float(input("Enter number of hours worked: ")) # Hours worked
+    overtime_hours = float(input("Enter number of overtime hours worked: ")) # Overtime hours worked
+    hourly_pay = float(input("Enter hourly pay rate: ")) # Hourly pay rate
 if hours_worked > 40:
     print("THE MAX IS 40")
     
-    regular_pay = hours_worked * hourly_pay
-    overtime_pay = overtime_hours * hourly_pay * 1.5
-    total_pay = regular_pay + overtime_pay
+    regular_pay = hours_worked * hourly_pay # Regular pay
+    overtime_pay = overtime_hours * hourly_pay * 1.5 # Overtime pay
+    total_pay = regular_pay + overtime_pay # Total pay
 
-    print(f"{employee_num}'s total pay: ${total_pay:.2f}")
+    print(f"{employee_num}'s total pay: ${total_pay:.2f}") # Display total pay
     total_payroll += total_pay
 
 
