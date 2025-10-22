4bb2ea6ba14b57206565c5c541857646ba15250c
Donyboi <djean2@brooklynsteamcenter.org>
2025-10-22 10:50:52 -0400

    yooo

---
+++ 13_classical_classes.py
@@ -0,0 +1,53 @@
+"""
+Task 1: People Class
+Define a class called Person with attributes name and age.
+Then, write a method within the class to introduce the person with their name and age.
+
+Create a new object using this new class, and call the method you created.
+"""
+print("----task 1 -------")
+
+class Person:
+    species = 'human'
+   
+
+    def __init__(self, name, age):
+     self.name = name
+     self.age = age
+
+dony = Person("Dony" ,17 )
+print(dony.name)
+print(dony.age)
+
+
+"""
+Task 2: Animals Speak
+Create a base class Animal with a empty method called speak. 
+
+Then create two subclasses, Dog and Cat, each with their own speak method. 
+
+Create objects using these subclasses and call the speak method.
+"""
+print("----taks 2------")
+
+class Animal:
+    def speak(self):
+        pass  # Placeholder method
+
+    class Dog(Animal):
+        def speak(self):
+            print("bark")
+
+        
+
+                 
+      
+
+"""
+Task 3: Banking
+Create a class BankAccount with attributes balance and owner. 
+
+Include methods for depositing and withdrawing money, which should modify the balance attribute.
+
+Test these methods with instances of the class.
+"""
